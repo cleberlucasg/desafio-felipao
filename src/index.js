38e72c9e1accd7366004f2b07e0862ee3c31a706
cleberlucasg <cleber.lucasg@gmail.com>
2024-03-22 15:25:26 -0300

    Projeto do Heroi

--- src/index.js
+++ src/index.js
@@ -1,2 +1,34 @@
-let nome = "Cléber";
-console.log(nome);
+let nomeDoHeroi = "Cléber";
+let xpDoHeroi = 8400
+
+if (xpDoHeroi<1000){
+  console.log("O Herói de nome " + (nomeDoHeroi) + " está no nível Ferro")
+}
+
+if (xpDoHeroi>1001&&xpDoHeroi<2000){
+    console.log("O Herói de nome " + (nomeDoHeroi) + " está no nível Bronze")
+  }
+
+if (xpDoHeroi>2001&&xpDoHeroi<5000){
+    console.log("O Herói de nome " + (nomeDoHeroi) + " está no nível Prata")
+  }
+  
+if (xpDoHeroi>5001&&xpDoHeroi<7000){
+    console.log("O Herói de nome " + (nomeDoHeroi) + " está no nível Ouro")
+  }
+
+if (xpDoHeroi>7001&&xpDoHeroi<8000){
+    console.log("O Herói de nome " + (nomeDoHeroi) + " está no nível Platina")
+  }
+
+if (xpDoHeroi>8001&&xpDoHeroi<9000){
+    console.log("O Herói de nome " + (nomeDoHeroi) + " está no nível Ascedente")
+  }
+
+if (xpDoHeroi>9001&&xpDoHeroi<10000){
+    console.log("O Herói de nome " + (nomeDoHeroi) + " está no nível Imortal")
+  }
+
+if (xpDoHeroi>=10001){
+    console.log("O Herói de nome " + (nomeDoHeroi) + " está no nível Radiante")
+  }
